bbe46779b872351380461cee944b61d398b7683c
Greg Trevellick <gtrevellick@googlemail.com>
2017-10-21 08:06:30 +0100

    .

---
+++ src/GeekQuiz.Ui/Options/HiddenOptions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+
+namespace Trivial.Ui.GeekQuiz.Options
+{
+    public class HiddenOptions : DialogPage
+    {
+        public DateTime LastPopUpDateTime { get; set; }
+        public int PopUpCountToday { get; set; }
+        public int TotalQuestionsAnsweredCorrectly { get; set; }
+        public int TotalQuestionsAsked { get; set; }
+    }
+}
---
+++ src/GeekQuiz.Ui/VSPackage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace GeekQuiz.Ui
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(VSPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class VSPackage : Package
+    {
+        public const string PackageGuidString = "32f71011-b391-4de0-bf08-ebc3a93c511e";
+
+        public VSPackage()
+        {
+        }
+
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+    }
+}
