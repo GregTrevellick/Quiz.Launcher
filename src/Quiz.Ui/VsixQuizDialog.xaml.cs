ddc0691882fc529a7c546e65022519d7e44a4fa7
Greg Trevellick <gtrevellick@googlemail.com>
2017-10-30 18:25:21 +0000

    .

--- src/Quiz.Ui/VsixQuizDialog.xaml.cs
+++ src/Quiz.Ui/VsixQuizDialog.xaml.cs
@@ -9,6 +9,12 @@ namespace Quiz.Ui
 {
     public partial class VsixQuizDialog : UserControl
     {
+
+        //https://stackoverflow.com/questions/18913208/colors-in-visual-studio-extension
+        //https://docs.microsoft.com/en-us/dotnet/api/microsoft.visualstudio.platformui.environmentcolors?redirectedfrom=MSDN&view=visualstudiosdk-2017
+
+
+
         public string CorrectAnswer;
         private readonly string _optionsName;
         public QuestionType QuestionType;
