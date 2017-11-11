19b604fa30a66a24fa640a625e9e20e3f8c362fa
Greg Trevellick <gtrevellick@googlemail.com>
2017-11-11 18:45:33 +0000

    .

---
+++ src/Quiz.UnitTests/CommonTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Quiz.Questions;
+
+namespace Quiz.UnitTests
+{
+    [TestClass]
+    public class CommonTests
+    {
+        [TestMethod]
+        public void UppercaseFirstTest()
+        {
+            Assert.AreEqual("Abc", Common.UppercaseFirst("abc"));
+        }
+    }
+}
---
+++ src/Quiz.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quiz.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quiz.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3b117e7-92ca-435c-a279-4cd302ab26ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
