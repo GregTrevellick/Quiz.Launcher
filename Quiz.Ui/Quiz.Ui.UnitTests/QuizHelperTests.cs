9d858fa31127efe8bcb28830f5a208c1066eee21
Greg Trevellick <gtrevellick@googlemail.com>
2017-11-12 07:52:06 +0000

    .

--- Quiz.Ui/Quiz.Ui.UnitTests/QuizHelperTests.cs
+++ Quiz.Ui/Quiz.Ui.UnitTests/QuizHelperTests.cs
@@ -5,10 +5,32 @@ namespace Quiz.Ui.UnitTests
     [TestClass]
     public class QuizHelperTests
     {
-        [TestMethod]
-        public void GetPercentageSuccess2Test()
+        [DataRow(5, 20, 25)]
+        [DataRow(1, 1, 100)]
+        [DataTestMethod]
+        public void GetPercentageSuccess2Test(int? totalQuestionsAnsweredCorrectly, int? totalQuestionsAsked, int expected)
         {
-            Assert.AreEqual(25, QuizHelper.GetPercentageSuccess2(5, 20));
+            Assert.AreEqual(expected, QuizHelper.GetPercentageSuccess2(totalQuestionsAnsweredCorrectly, totalQuestionsAsked));
         }
     }
 }
+
+
+
+
+
+//[TestClass]
+//public class FibonacciTests
+//{
+//    [DataRow(0, 0)]
+//    [DataRow(1, 1)]
+//    [DataRow(2, 1)]
+//    [DataRow(80, 23416728348467685)]
+//    [DataTestMethod]
+//    public void GivenDataFibonacciReturnsResultsOk(int number, Int64 result)
+//    {
+//        var fib = new Fib();
+//        var actual = fib.Fibonacci(number);
+//        Assert.AreEqual(result, actual);
+//    }
+//}
