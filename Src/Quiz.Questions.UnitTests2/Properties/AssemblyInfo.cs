26491674212eaff2b5195c5803afcc64a797d046
Greg Trevellick <gtrevellick@googlemail.com>
2017-11-15 21:58:52 +0000

    .

---
+++ Src/Quiz.Questions.UnitTests2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quiz.Questions.UnitTests2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quiz.Questions.UnitTests2")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b5330e2-5c50-4afa-82ab-f328c7258b00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Src/Quiz.Questions.UnitTests2/QuestionsOpenTdbGatewayTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Quiz.Questions.UnitTests
+{
+    [TestClass]
+    public class QuestionsOpenTdbGatewayTests
+    {
+        [TestMethod]
+        public void GetQuestionTypeTest()
+        {
+            Assert.AreEqual(QuestionType.MultiChoice, QuestionsOpenTdbGateway.GetQuestionType(null));
+            Assert.AreEqual(QuestionType.MultiChoice, QuestionsOpenTdbGateway.GetQuestionType(new Result()));
+            Assert.AreEqual(QuestionType.MultiChoice, QuestionsOpenTdbGateway.GetQuestionType(new Result{type=""}));
+            Assert.AreEqual(QuestionType.TrueFalse, QuestionsOpenTdbGateway.GetQuestionType(new Result{type="boolean"}));
+            Assert.AreEqual(QuestionType.TrueFalse, QuestionsOpenTdbGateway.GetQuestionType(new Result { type = "Boolean" }));
+            Assert.AreEqual(QuestionType.TrueFalse, QuestionsOpenTdbGateway.GetQuestionType(new Result { type = "booleaN" }));
+        }
+    }
+}
